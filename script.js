7be37d9f8f7c7ceeed5d1807dcc9b601274cb936
abdirisakhd <abdirisakjr@gmail.com>
2021-09-07 11:17:16 +0300

    add score variables

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let options = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
+  let options = ["rock", "paper", "scissors"];
   let index = Math.floor(Math.random() * 3);
   return options[index];
 }
@@ -8,28 +10,40 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   let player = playerSelection.toLowerCase().trim();
   let computer = computerSelection.toLowerCase();
-  if (player == "rock" && computer == "paper")
-    return "you lose, paper beat rock";
-  else if (player == "rock" && computer == "scissors")
-    return "you win, rock beat scissors";
-  else if (player == "rock" && computer == "rock") return "it's tie";
-  else if (player == "paper" && computer == "rock")
-    return "you win, paper beat rock";
-  else if (player == "paper" && computer == "scissors")
-    return "you lose, scissors cut paper";
+  if (player == "rock" && computer == "paper") {
+    computerScore++;
+    return "Lose, paper beat rock";
+  } else if (player == "rock" && computer == "scissors") {
+    playerScore++;
+    return "Win, rock beat scissors";
+  } else if (player == "paper" && computer == "rock") {
+    playerScore++;
+    return "Win, paper beat rock";
+  } else if (player == "paper" && computer == "scissors") {
+    computerScore++;
+    return "Lose, scissors cut paper";
+  } else if (player == "scissors" && computer == "rock") {
+    computerScore++;
+    return "Lose, rock beat scissors";
+  } else if (player == "scissors" && computer == "paper") {
+    playerScore++;
+    return "Win, scissors cut paper";
+  } else if (player == "rock" && computer == "rock") return "it's tie";
   else if (player == "paper" && computer == "paper") return "it's tie";
-  else if (player == "scissors" && computer == "rock")
-    return "you lose, rock beat scissors";
-  else if (player == "scissors" && computer == "paper")
-    return "you win, scissors cut paper";
   else if (player == "scissors" && computer == "scissors") return "it's tie";
   else return "wrong input";
 }
-let count = 1;
-function game() {
-  let userInput = prompt("choose rock, paper or scissors:");
-  console.log(playRound(userInput, computerPlay()));
 
-  count += 0;
-  if (count < 5) game();
+function game() {
+  while (playerScore < 3 && computerScore < 3) {
+    let userInput = prompt("choose rock, paper or scissors:");
+    let play = playRound(userInput, computerPlay());
+
+    if (play == "wrong input") {
+      console.log("wrong input, pls choose from the above");
+      continue;
+    }
+    console.log(play);
+    console.log(`player ${playerScore}-${computerScore} computer`);
+  }
 }
